d3adca87ea8a6e82fa3967ba31f964bc76413d82
Jort van Gorkum <jortvangorkum@gmail.com>
2022-04-01 15:21:07 +0200

    Working linear plot

--- generate-graphs/generate_graphs/main.py
+++ generate-graphs/generate_graphs/main.py
@@ -1,18 +1,21 @@
+from typing import List
+import numpy as np
 import pandas as pd
 import re
 import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib.axes import Axes
 from scipy import stats
+import statsmodels.api as sm
 
-regex = r"benchmarking (.*?)\/(\d*)(?:(?:.|\n)*?)iters(?:(?:\s)*)(\d+\.?\d+e?\d*)"
+regex_memory_benchmark = r"benchmarking (.*?)\/(\d*)(?:(?:.|\n)*?)iters(?:(?:\s)*)(\d+\.?\d+e?\d*)"
 
 DATA_MEMORY_PATH = './data/memory'
 
 def parse_memory_data_file(file_name: str) -> pd.DataFrame:
     with open(f"{DATA_MEMORY_PATH}/{file_name}.txt", 'r') as data_memory_file:
         data_memory_contents = data_memory_file.read()
-        matches = re.finditer(regex, data_memory_contents)
+        matches = re.finditer(regex_memory_benchmark, data_memory_contents)
         df_data_memory = pd.DataFrame(columns=['Benchmark', 'Amount Nodes', 'Allocated Bytes'])
 
         for match in matches:
@@ -27,29 +30,41 @@ def parse_memory_data_file(file_name: str) -> pd.DataFrame:
 
         return df_data_memory
 
-def plot_benchmark(data: pd.DataFrame) -> Axes:
-    (slope, intercept, r_value, _, _) = stats.linregress(data["Amount Nodes"], data["Allocated Bytes"])
-    
-    ax = sns.regplot(
-        x="Amount Nodes", 
-        y="Allocated Bytes", 
-        data=data, 
-        line_kws={
-            'label': "y={0:.1f}x+{1:.1f}, R²={2:.2f}".format(slope, intercept, r_value * r_value)
-            }
-    )
-    ax.set_xscale('log')
-    ax.set_yscale('log')
-    ax.legend()
+def plot_linear_benchmark(df: pd.DataFrame, benchmark_name: str, logx = True, logy = True) -> None:
+    data = df[df['Benchmark'] == benchmark_name]
+    x = data["Amount Nodes"]
+    y = data["Allocated Bytes"]
 
-    return ax
+    model = sm.OLS(y, x)
+    r = model.fit()
+    slope = r.params[0]
+    rsquared = r.rsquared 
+    
+    ax1 = sns.lineplot(x=x, y=slope * x)
+    ax2 = sns.scatterplot(x=x, y=y)
+
+    if logx:
+        plt.xscale('log')
+    if logy:
+        plt.yscale('log')
+    
+    ax1.legend(title="Equation", loc='upper left', labels=["y={0:.1f}x, R²={1:.2f}".format(slope, rsquared)])
 
 
 if __name__ == "__main__":
-    df_data_memory = parse_memory_data_file('run-0')
-    print(df_data_memory)
-
     sns.set_palette("pastel")
 
-    ax_data_memory = plot_benchmark(df_data_memory)
+    df_data_memory_1 = parse_memory_data_file('run-1')
+    # print(df_data_memory_1)
+
+    plot_linear_benchmark(df_data_memory_1, 'Cata Sum Memory')
     plt.show()
+    plt.clf()
+
+    plot_linear_benchmark(df_data_memory_1, 'Generic Cata Sum')
+    plt.show()
+    plt.clf()
+
+    plot_linear_benchmark(df_data_memory_1, 'Incremental Compute Map')
+    plt.show()
+    plt.clf()
