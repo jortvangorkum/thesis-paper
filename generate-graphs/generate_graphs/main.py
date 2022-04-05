05dc546d51a12da21096cc52aab98a55dcf64435
Jort van Gorkum <jortvangorkum@gmail.com>
2022-04-05 11:17:21 +0200

    Got here

--- generate-graphs/generate_graphs/main.py
+++ generate-graphs/generate_graphs/main.py
@@ -1,4 +1,5 @@
-from typing import List
+from enum import Enum, auto
+from typing import List, Tuple
 import numpy as np
 import pandas as pd
 import re
@@ -12,6 +13,10 @@ regex_memory_benchmark = r"benchmarking (.*?)\/(\d*)(?:(?:.|\n)*?)iters(?:(?:\s)
 
 DATA_MEMORY_PATH = './data/memory'
 IMAGES_PATH = '../images'
+
+class FuncType(Enum):
+    Linear = auto()
+    Log = auto()
 
 def calculate_rsquared(x: pd.Series, y: pd.Series, f, popt) -> float:
     ss_res   = np.dot((y - f(x, *popt)), (y - f(x, *popt)))
@@ -39,29 +44,28 @@ def parse_memory_data_file(file_name: str) -> pd.DataFrame:
 
         return df_data_memory
 
-def plot_linear_benchmark(df: pd.DataFrame, benchmark_name: str) -> None:
-    data = df[df['Benchmark'] == benchmark_name]
-    x = data["Amount Nodes"]
-    y = data["Allocated Bytes"]
-
+def plot_linear_line(x: pd.Series, y: pd.Series) -> None:
     model = sm.OLS(y, x)
     r = model.fit()
     slope = r.params[0]
     rsquared = r.rsquared 
 
     ax1 = sns.lineplot(x=x, y=slope * x)
-    ax2 = sns.scatterplot(x=x, y=y)
-
-    plt.xscale('log')
-    plt.yscale('log')
     
     ax1.legend(title="Equation", loc='upper left', labels=["y={0:.1f}x, R²={1:.2f}".format(slope, rsquared)])
 
-def plot_log_benchmark(df: pd.DataFrame, benchmark_name: str) -> None:
+def plot_linear_benchmark(df: pd.DataFrame, benchmark_name: str) -> None:
     data = df[df['Benchmark'] == benchmark_name]
     x = data["Amount Nodes"]
     y = data["Allocated Bytes"]
 
+    plot_linear_line(x, y)
+    ax2 = sns.scatterplot(x=x, y=y)
+
+    plt.xscale('log')
+    plt.yscale('log')
+
+def plot_log_line(x: pd.Series, y: pd.Series) -> None:
     def log_func(x, a, b): 
         return a + b * np.log(x)
 
@@ -69,12 +73,38 @@ def plot_log_benchmark(df: pd.DataFrame, benchmark_name: str) -> None:
     rsquared = calculate_rsquared(x, y, log_func, popt)
 
     ax1 = sns.lineplot(x=x, y=x.apply(lambda i: log_func(i, *popt)))
+    
+    ax1.legend(title="Equation", loc='upper left', labels=["y={0:.1f} * ln(x) + {1:.1f}, R²={2:.2f}".format(*popt, rsquared)])
+
+def plot_log_benchmark(df: pd.DataFrame, benchmark_name: str) -> None:
+    data = df[df['Benchmark'] == benchmark_name]
+    x = data["Amount Nodes"]
+    y = data["Allocated Bytes"]
+
+    plot_log_line(x, y)
     ax2 = sns.scatterplot(x=x, y=y)
 
     plt.xscale('log')
     plt.ticklabel_format(axis='y', style='sci', scilimits=(0,3), useMathText=True)
 
-    ax1.legend(title="Equation", loc='upper left', labels=["y={0:.1f} * ln(x) + {1:.1f}, R²={2:.2f}".format(*popt, rsquared)])
+def plot_all_benchmarks(df: pd.DataFrame, benchmark_names: List[Tuple[str, FuncType]]) -> None:
+    for (benchmark_name, func_type) in benchmark_names:
+        data = df[df['Benchmark'] == benchmark_name]
+        x = data["Amount Nodes"]
+        y = data["Allocated Bytes"]
+
+        match func_type:
+            case FuncType.Linear:
+                plot_linear_line(x, y)
+            case FuncType.Log:
+                plot_log_line(x,y)
+
+        ax = sns.scatterplot(x=x, y=y)
+
+        plt.xscale('log')
+        plt.yscale('log')
+
+        ax.legend(title="Benchmarks", loc="upper left", labels=benchmark_names)
 
 def save_benchmark(file_name: str) -> None:
     plt.savefig(f'{IMAGES_PATH}/{file_name}.pdf')
@@ -93,3 +123,6 @@ if __name__ == "__main__":
 
     plot_log_benchmark(df_data_memory_1, 'Incremental Compute Map')
     save_benchmark('plots/memory/benchmark_incremental_cata_sum')
+
+    plot_all_benchmarks(df_data_memory_1, ['Cata Sum Memory', 'Generic Cata Sum', 'Incremental Compute Map'])
+    save_benchmark('plots/memory/all_benchmarks')
