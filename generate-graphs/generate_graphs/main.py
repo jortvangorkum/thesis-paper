50213d2aaf7395de9d662b7514a32f4ae10d8f7b
Jort van Gorkum <jortvangorkum@gmail.com>
2022-06-14 15:32:35 +0200

    New benchmarks

--- generate-graphs/generate_graphs/main.py
+++ generate-graphs/generate_graphs/main.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
         images_path='../images/plots',
         df_mem_path='./data/memory',
         df_time_path='./data/time',
-        runs=['run-1']
+        runs=['run-3']
     )
 
-    plotter.plot_run_benchmarks('run-1')
+    plotter.plot_run_benchmarks('run-3')
     # plotter.plot_comparison_runs(['run-4', 'run-5'])
